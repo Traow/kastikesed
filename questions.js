58ae0474c7eae63a423b349636f8b1f66b722add
Robi Mustsaar <robimustsaar@gmail.com>
2022-11-15 10:28:36 +0200

    lisasin kysimusi

--- questions.js
+++ questions.js
@@ -8,10 +8,14 @@ inquirer
         {type:'list', name:'list', message:'what is ur fav color?', choices:['red', 'green', 'blue']},
         {type:'rawlist', name:'rawlist', message:'How many people are you living with?', choices:['1', '2', '3','4+']},
         {type:'checkbox', name:'checkbox', message:'Check what numbers you like?', choices: ['1', '2', '3','4']},
+
+        {type:'expand', name:'expand', message:'yes or no', choices:[{key:'y',value:'yes'}, {key:'n',value:'no',},],},
         
         {type:'password', name:'password', message:'What is ur password?'},
         {type:'input', name:'location', message:'Where are you now?'},
         {type:'confirm', name:'confirm', message:'Are you scared?'},
+
+        {type:'editor', name:'editor', message:'Enter a description'}
 
 
         
@@ -22,7 +26,9 @@ inquirer
         console.log(`My fav color is ${answers.list}`);
         console.log(`You live with ${answers.rawlist} people`);
         console.log(`You like ${answers.checkbox}`);
+        console.log(`Yes or no? ${answers.expand}`);
         console.log(`My password is ${answers.password}`);
         console.log(`Right now I am at ${answers.location}`);
         console.log(`Am I scared? ${answers.confirm}`);
+        console.log(`you wrote  ${answers.editor}`);
     })
